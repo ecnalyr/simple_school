ced7f5ecaadee4302003d4c525ae3450ea2b48b2
ecnalyr <ecnalyr@gmail.com>
2015-01-30 12:30:56 -0600

    Created has and belongs to many relationship between students and classrooms.
    - I can't imagine a scenario where I'd have to deal with the relationship model separately,
    so I used habtm rather than has_many :through (relationship_model).

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150130175954) do
+ActiveRecord::Schema.define(version: 20150130182819) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,6 +21,13 @@ ActiveRecord::Schema.define(version: 20150130175954) do
     t.datetime "created_at"
     t.datetime "updated_at"
   end
+
+  create_table "classrooms_students", id: false, force: true do |t|
+    t.integer "classroom_id"
+    t.integer "student_id"
+  end
+
+  add_index "classrooms_students", ["classroom_id", "student_id"], name: "classrooms_students_index", unique: true, using: :btree
 
   create_table "students", force: true do |t|
     t.string   "name"
