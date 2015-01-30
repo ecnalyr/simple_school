a8b32a78652a7cabda799a4e2f9c109decc2e82f
ecnalyr <ecnalyr@gmail.com>
2015-01-30 16:59:15 -0600

    Created visitor_can_assign_homework_to_a_student spec.
    - Extracted similar test prep to a before :each block

--- spec/features/visitors/interact_with_available_assignments_spec.rb
+++ spec/features/visitors/interact_with_available_assignments_spec.rb
@@ -3,18 +3,35 @@
 #   I want to see assignments a student can claim
 #   So I can see what assignments a student can claim
 feature "Interact with assignments associated with a student's classrooms" do
+  before :each do
+    @student = create(:student, name: 'Gregory House')
+    @classroom = @student.classrooms.create(name: 'Nephrology')
+    @assignment = @classroom.assignments.create(name: 'Kidney Extraction')
+  end
+
   # Scenario: No complete assignments / homework
   #   Given I am a visitor
   #   When I view a student
   #   Then I see the assignments available to that student in a classroom.
   scenario 'visitor can see assignments from a classroom' do
-    student = create(:student, name: 'Gregory House')
-    classroom = student.classrooms.create(name: 'Nephrology')
-    classroom.assignments.create(name: 'Kidney Extraction')
+    visit student_path(@student)
 
-    visit student_path(student)
+    within "#classroom-#{@classroom.id}" do
+      expect(page).to have_content 'Kidney Extraction'
+    end
+  end
+  # Scenario: No complete assignments / homework
+  #   Given I am a visitor
+  #   When I assign an assignment as homework to a student
+  #   Then I see the homework assigned to the student.
+  scenario 'visitor can assign homework to a student' do
+    visit student_path(@student)
 
-    within "#classroom-#{classroom.id}" do
+    within "#assignment-#{@assignment.id}" do
+      click_link 'assign as homework'
+    end
+
+    within "#classroom-#{@classroom.id}.homework" do
       expect(page).to have_content 'Kidney Extraction'
     end
   end
